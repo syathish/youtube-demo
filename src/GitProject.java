02f01666b7ad5a395cc51ded43f871b0e4ca1753
syathish <syathish9@gmail.com>
2014-11-18 13:04:01 +0000

    moar changes to index

--- src/GitProject.java
+++ src/GitProject.java
@@ -3,5 +3,6 @@ public class GitProject {
 public static void main(String[] args) {
     System.out.println("Hello, this is a test of GitHub");
     System.out.println("This program has been modified.");
+    System.out.println("Moar and Moar test code");
 }
 }
